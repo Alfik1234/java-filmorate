c2380e4bdb055d428d0cf2cc2636db292311b18b
Alfik <Alfik1234@yandex.ru>
2026-03-16 20:11:47 +0300

    Исправить функционал exception

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -2,8 +2,9 @@ package ru.yandex.practicum.filmorate.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
 
 import jakarta.validation.Valid;
 import java.time.LocalDate;
@@ -17,49 +18,43 @@ public class FilmController {
     private final Map<Long, Film> films = new HashMap<>();
     private Long nextId = 1L;
 
-    private static final LocalDate CINEMA_BIRTHDAY = LocalDate.of(1895, 12, 28);
-
     private Long getNextId() {
         return nextId++;
     }
 
-    private void validateFilm(Film film) {
-        if (film.getReleaseDate().isBefore(CINEMA_BIRTHDAY)) {
-            log.error("Дата релиза раньше 28.12.1895");
-            throw new ValidationException("Дата релиза не может быть раньше 28 декабря 1895 года");
-        }
-    }
-
     @PostMapping
-    public Film addFilm(@Valid @RequestBody Film film) {
+    public Film createFilm(@Valid @RequestBody Film film) {
 
-        validateFilm(film);
+        if (film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
+            throw new ValidationException("Дата релиза не может быть раньше 28 декабря 1895");
+        }
 
         film.setId(getNextId());
         films.put(film.getId(), film);
 
-        log.info("Добавлен фильм: {}", film.getName());
+        log.info("Добавлен фильм {}", film);
         return film;
     }
 
     @PutMapping
     public Film updateFilm(@Valid @RequestBody Film film) {
 
-        if (!films.containsKey(film.getId())) {
-            log.error("Фильм с id {} не найден", film.getId());
-            throw new ValidationException("Фильм с id " + film.getId() + " не найден");
+        if (film.getId() == null || !films.containsKey(film.getId())) {
+            throw new NotFoundException("Фильм не найден");
         }
 
-        validateFilm(film);
+        if (film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
+            throw new ValidationException("Дата релиза не может быть раньше 28 декабря 1895");
+        }
 
         films.put(film.getId(), film);
 
-        log.info("Обновлён фильм: {}", film.getName());
+        log.info("Обновлён фильм {}", film);
         return film;
     }
 
     @GetMapping
-    public List<Film> getAllFilms() {
+    public List<Film> getFilms() {
         log.info("Получен список фильмов");
         return new ArrayList<>(films.values());
     }
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -2,8 +2,9 @@ package ru.yandex.practicum.filmorate.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.User;
 
 import jakarta.validation.Valid;
 import java.util.*;
@@ -30,16 +31,15 @@ public class UserController {
         user.setId(getNextId());
         users.put(user.getId(), user);
 
-        log.info("Создан пользователь {}", user.getLogin());
+        log.info("Создан пользователь {}", user);
         return user;
     }
 
     @PutMapping
     public User updateUser(@Valid @RequestBody User user) {
 
-        if (!users.containsKey(user.getId())) {
-            log.error("Пользователь с id {} не найден", user.getId());
-            throw new ValidationException("Пользователь не найден");
+        if (user.getId() == null || !users.containsKey(user.getId())) {
+            throw new NotFoundException("Пользователь не найден");
         }
 
         if (user.getName() == null || user.getName().isBlank()) {
@@ -48,7 +48,7 @@ public class UserController {
 
         users.put(user.getId(), user);
 
-        log.info("Обновлён пользователь {}", user.getLogin());
+        log.info("Обновлён пользователь {}", user);
         return user;
     }
 
